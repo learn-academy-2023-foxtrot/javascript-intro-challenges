dea3f43f035d34e664472d2a42b9871fbecc570f
jdkevwitch <plainodyssey@gmail.com>
2023-08-30 16:37:17 -0700

    Did challenges 9 through 13

--- conditionals-ilene-xena-josh.js
+++ conditionals-ilene-xena-josh.js
@@ -15,3 +15,27 @@ console.log(queSeven.includes("RN"))
 // Output - True
 console.log(queSeven.includes("rn"))
 // Output - False
+console.log("LEARN"[0] === "l")
+// Output - False
+console.log("LEARN"[0] === "L")
+// Output - True
+var myMoney = 50
+if (myMoney <= 100) {
+    console.log("in budget")
+}
+// Output - in budget
+var imHungry = "Im Hungry"
+if (imHungry === "Im Hungry") {
+    console.log("eat food");
+} else  {
+    console.log("keep coding")
+}
+var trafficLight = "Green"
+var trafficLightTwo = "Yellow"
+if (trafficLight === "Green") {
+    console.log("Go!")
+} else if (trafficLightTwo === "Yellow") {
+    console.log("Slow Down!")
+} else {
+    console.log("Stop!")
+}
