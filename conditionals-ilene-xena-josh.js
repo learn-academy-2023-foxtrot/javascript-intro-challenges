226fd397f18bdc26a96478504e93638298f886c1
XenaSit <sitnikovaxena@gmail.com>
2023-08-30 16:07:17 -0700

    did quastions 5 through 8

--- conditionals-ilene-xena-josh.js
+++ conditionals-ilene-xena-josh.js
@@ -6,3 +6,12 @@ console.log(5 === "5")
 //Output - False
 console.log(!3 === 3)
 //Output - False
+console.log("LEARN".length === 5 && "Student".length === 7)
+// Output - True
+console.log("LEARN".length === 5 || "Student".length === 10)
+// Output - True
+var queSeven = "LEARN"
+console.log(queSeven.includes("RN"))
+// Output - True
+console.log(queSeven.includes("rn"))
+// Output - False
