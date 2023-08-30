e91c7da42d398fc9d0fa6e7b8d9b4063c3ef08f9
tori-calkins <tori.calkins2705@gmail.com>
2023-08-30 11:49:43 -0700

    next 3 answers

--- intro-aleja-tori-adrian.js
+++ intro-aleja-tori-adrian.js
@@ -9,7 +9,10 @@
 // Write the code that will log the output of 5 to the power of 7.
     console.log(5 ** 7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+    console.log(33 % 6)
 // Write the code that will log the length of a string containing your name.
+    console.log("tori".length)
 // Write the code that will log whether your string includes the letter "e"?
+    console.log("tori".includes("e"))
 // Write the code that will log the character at the first index of the string.
 // Write the code that will log the string in all uppercase letters.
