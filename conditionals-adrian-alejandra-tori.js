5522014f1c480a191329606b7f03341dc780eedf
tori-calkins <tori.calkins2705@gmail.com>
2023-08-30 15:49:02 -0700

    next three answers

--- conditionals-adrian-alejandra-tori.js
+++ conditionals-adrian-alejandra-tori.js
@@ -9,9 +9,11 @@ console.log(5 === "5")
 // Does !3 strictly equal 3?
 console.log(!3 === 3);
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-
+console.log("LEARN".length === 5 && "Student".length === 7);
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10);
 // Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"));
 // Does "LEARN" contain the subset "rn"?
 // Does "LEARN"[0] strictly equal "l"?
 // Modify the code from the previous question to return true.
