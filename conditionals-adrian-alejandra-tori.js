81b8abea966ee0d4ee75ebde246bfa8c5a801b15
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-08-30 16:15:32 -0700

    next few changes

--- conditionals-adrian-alejandra-tori.js
+++ conditionals-adrian-alejandra-tori.js
@@ -27,6 +27,23 @@ if (item <= 100) {
     console.log("in budget");
 }
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var hungry = true
+if (hungry === true) {
+    console.log("eat food");
+} else {
+    console.log("keep coding!!");
+};
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red
+var trafficLight = "yellow";
+if (trafficLight === "green") {
+    console.log("go");
+} else if (trafficLight === "yellow") {
+    console.log("slow down");
+} else if (trafficLight === "red") {
+    console.log("stop");
+} 
+
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
