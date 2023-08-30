dd066f7e1222432f5f65874217521ce4ecffdfc0
alopezdelgado <andrealdelgado_99@outlook.com>
2023-08-30 16:36:44 -0700

    Completed challenges 6-8 of conditionals

--- conditionals-anabella-dre.js
+++ conditionals-anabella-dre.js
@@ -10,7 +10,11 @@
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
     console.log("LEARN" .length === 5 && "Student" .length === 7) //Output= true
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+    console.log("LEARN" .length === 5 || "Student" .length === 10) //Output= true
 // Does "LEARN" contain the subset "RN"?
+    var myString = "LEARN"
+    console.log(myString.includes("RN")) //Output= true
 // Does "LEARN" contain the subset "rn"?
+    
 // Does "LEARN"[0] strictly equal "l"?
 // Modify the code from the previous question to return true.
