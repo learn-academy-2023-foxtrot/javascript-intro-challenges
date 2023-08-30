cd0a6a9356943fedf221b9e5f8633605b6f6225a
RonielOverton <ronielo86@gmail.com>
2023-08-30 16:23:23 -0700

    added conditionals

---
+++ conditionals-roniel-shaun.js
@@ -0,0 +1,39 @@
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34/3 > 67/2);
+// output false
+
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
+// output true
+
+
+// Does 5 strictly equal "5"?
+console.log(5 === "5")
+//output fasle
+
+// Does !3 strictly equal 3?
+console.log(!3 === 3)
+//output fasle
+
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
+//output true
+
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
+//output true
+
+// Does "LEARN" contain the subset "RN"?
+console.log("LEARN")
+
+// Does "LEARN" contain the subset "rn"?
+
+// Does "LEARN"[0] strictly equal "l"?
+
+console.log("LEARN"[0] === "l")
+//output fasle
+
+// Modify the code from the previous question to return true.
+console.log("lEARN" [0] === "l")
+//output true
+
