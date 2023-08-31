b2cfc7d312edc283fba413d525358e04e53bfe51
AlejaH1213 <alejajara2@gmail.com>
2023-08-31 11:45:51 -0700

    second set of challenges

--- arrays-anabella-aleja-xena.js
+++ arrays-anabella-aleja-xena.js
@@ -12,3 +12,17 @@ console.log(sliceArray)
 // Write the code that will add "beans" to the "chips" and "dip" array.
 sliceArray.push("beans")
 console.log(sliceArray)
+
+var numbers= [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+numbers.unshift(0)
+console.log(numbers)
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
+// Write the code that will remove the first number from the array.
+numbers.shift(0)
+console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var newNumber= [0]
+console.log(newNumber.concat(numbers))
