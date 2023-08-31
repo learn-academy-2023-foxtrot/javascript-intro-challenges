c74646fe47953c2606aed4625dcb59e116423752
RonielOverton <ronielo86@gmail.com>
2023-08-31 16:57:46 -0700

    added challenge changed

--- loops-roniel-mal.js
+++ loops-roniel-mal.js
@@ -17,11 +17,44 @@
 //     console.log(arr[i] * 3)
 //  }
 
-// Create a for loop that logs each even number from 1-20 and in the place of every odd number returns the word ODD.
 // for(let i = 0; i < 20; i++) {
-//     if (arr[i] % 2 !== 0){
-//         console.log("ODD")
-//     } else {
-//         console.log(arr[i])
-//     }
-// }
+    //     if (arr[i] % 2 !== 0){
+        //         console.log("ODD")
+        //     } else {
+            //         console.log(arr[i])
+            //     }
+            // }
+            
+            // const nums = [3, 57, -9, 20, 67]
+            // //create the code that will log the largest number from the array
+            // let highest = nums[0]
+            
+            // for( let i = 0; i < nums.length; i++){
+                //     if (nums[i] > highest)
+                //     highest = nums[i]
+                //     console.log();
+                // }
+                
+                // Create a for loop that logs each even number from 1-20 and in the place of every odd number returns the word ODD.
+        //         function largest(nums) {
+        //             let max = nums[0];
+        //             for( let i = 0; i < nums.length; i++) {
+        //                 if (nums[i] > max )
+        //                 max = nums[i];
+        // }
+        // return max;
+        // }
+        // const nums = [3, 57, -9, 20, 67]
+        // console.log(largest(nums))
+//output 67
+
+function smallest(nums) {
+    let max = nums[0];
+    for( let i = 0; i < nums.length; i--) {
+        if (nums[i] > max )
+        max = nums[i];
+}
+return max;
+}
+const nums = [3, 57, -9, 20, 67]
+console.log(smallest(nums))
