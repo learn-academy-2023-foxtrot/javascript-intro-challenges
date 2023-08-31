2b1df887e72a10feca522b220596af8bfbe12f88
jdkevwitch <plainodyssey@gmail.com>
2023-08-31 11:02:55 -0700

    questions 1-4

---
+++ arrays-josh-louis.js
@@ -0,0 +1,13 @@
+var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+groceryList.push("soda")
+console.log(groceryList)
+// Write the code that will add "granola" to the end of the array without altering the original array.
+groceryList.push("granola")
+console.log(groceryList)
+//
+groceryList.includes("chips", "dip")
+if (groceryList === true) {
+    console.log("chips", " dip")
+}
+//
