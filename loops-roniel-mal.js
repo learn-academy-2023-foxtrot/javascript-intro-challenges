ddc39def7ec16ef412ab34fa07f5c116befeffb8
malsherer <mgracesherer@gmail.com>
2023-08-31 15:43:24 -0700

    first challenges

---
+++ loops-roniel-mal.js
@@ -0,0 +1,27 @@
+// Create a for loop that logs each number from 1 to 20.]
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// for(let i = 0; i < 20; i++){
+// console.log(arr[i])
+// }
+
+// Create a for loop that logs every other number from 1 to 20.
+// for(let i = 0; i < 20; i++) {
+//     if (arr[i] % 2 !== 0){
+//         console.log(arr[i])
+//     }
+// }
+// Output: 1 3 5 7 9 11 13 15 17 19
+
+// Create a for loop that logs the result for each number from 1-20 tripled.
+// for(let i = 0; i < 20; i++){
+//     console.log(arr[i] * 3)
+//  }
+
+// Create a for loop that logs each even number from 1-20 and in the place of every odd number returns the word ODD.
+// for(let i = 0; i < 20; i++) {
+//     if (arr[i] % 2 !== 0){
+//         console.log("ODD")
+//     } else {
+//         console.log(arr[i])
+//     }
+// }
