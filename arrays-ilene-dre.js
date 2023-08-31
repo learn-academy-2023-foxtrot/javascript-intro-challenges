4595fe66f9ef4e630ca4782cd127cafd91e641db
alopezdelgado <andrealdelgado_99@outlook.com>
2023-08-31 11:13:53 -0700

    Did the second chunk of challenges

--- arrays-ilene-dre.js
+++ arrays-ilene-dre.js
@@ -16,3 +16,18 @@ console.log(groceryList.slice(0,2))
 // Write the code that will add "beans" to the "chips" and "dip" array.
 console.log(cutArray.push("beans"))
 console.log(cutArray)
+
+var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+numbers.unshift(0)
+console.log(numbers)
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
+// Write the code that will remove the first number from the array.
+numbers.shift()
+console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var zero = [0]
+console.log(zero.concat(numbers))
+
